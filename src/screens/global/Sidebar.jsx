1188c03b2dccd6a35408330e7be4cf05f25fe593
SultanKirigha <sultankirigha@gmail.com>
2023-06-12 14:43:34 +0300

    Added a Event Handler on the Side Bar to change the on click feature

--- src/screens/global/Sidebar.jsx
+++ src/screens/global/Sidebar.jsx
@@ -1,40 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "../../App"
 import { Briefcase, File, Home, Inbox, List, MessageCircle, Send, User, Users } from "react-feather";
 
 const Sidebar = () => {
+  const [activeMenuItem, setActiveMenuItem] = useState("/");
+
+  const handleMenuItemClick = (menuUrl) => {
+    setActiveMenuItem(menuUrl);
+  };
+
   return (
     <div className="flex">
       {/* BEGIN: Side Menu */}
       <nav className="side-nav">
-        <NavLink to="/" className="intro-x flex items-center pl-5 pt-4">
+        <a href="/" className="intro-x flex items-center pl-5 pt-4">
           <img alt="Midone Tailwind HTML Admin Template" className="w-6" src="dist/images/logo.svg" />
           <span className="hidden xl:block text-white text-lg ml-3">onthe<span className="font-medium"> table</span> </span>
-        </NavLink>
+        </a>
         <div className="side-nav__devider my-6"></div>
         <ul>
           <li>
-            <NavLink to="/" activeClassName="side-menu--active" className="side-menu">
+            <NavLink
+              to="/"
+              className={`side-menu ${activeMenuItem === "/" ? "side-menu--active" : ""}`}
+              onClick={() => handleMenuItemClick("/")}
+            >
               <div className="side-menu__icon"> <Home /> </div>
               <div className="side-menu__title"> Overview </div>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/personalProfile" activeClassName="side-menu--active" className="side-menu">
+            <NavLink
+              to="/personalProfile"
+              className={`side-menu ${activeMenuItem === "/personalProfile" ? "side-menu--active" : ""}`}
+              onClick={() => handleMenuItemClick("/personalProfile")}
+            >
               <div className="side-menu__icon"><User /> </div>
               <div className="side-menu__title"> Personal Profile </div>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/companyProfile" activeClassName="side-menu--active" className="side-menu">
+            <NavLink
+              to="/companyProfile"
+              className={`side-menu ${activeMenuItem === "/companyProfile" ? "side-menu--active" : ""}`}
+              onClick={() => handleMenuItemClick("/companyProfile")}
+            >
               <div className="side-menu__icon"> <Users /> </div>
               <div className="side-menu__title"> Company Profile </div>
             </NavLink>
           </li>
           <li className="side-nav__devider my-6"></li>
           <li>
-            <NavLink to="/inbox" activeClassName="side-menu--active" className="side-menu">
+            <NavLink
+              to="/inbox"
+              className={`side-menu ${activeMenuItem === "/inbox" ? "side-menu--active" : ""}`}
+              onClick={() => handleMenuItemClick("/inbox")}
+            >
               <div className="side-menu__icon"> <Inbox /> </div>
               <div className="side-menu__title"> Inbox </div>
             </NavLink>
@@ -46,13 +67,21 @@ const Sidebar = () => {
             </NavLink>
           </li>
           <li>
-            <NavLink to="/viewTalent" activeClassName="side-menu--active" className="side-menu">
+            <NavLink
+              to="/viewTalent"
+              className={`side-menu ${activeMenuItem === "/viewTalent" ? "side-menu--active" : ""}`}
+              onClick={() => handleMenuItemClick("/viewTalent")}
+            >
               <div className="side-menu__icon"> <File /> </div>
               <div className="side-menu__title"> View Talent </div>
             </NavLink>
           </li>
           <li>
-            <NavLink to="/dataList" activeClassName="side-menu--active" className="side-menu">
+            <NavLink
+              to="/dataList"
+              className={`side-menu ${activeMenuItem === "/dataList" ? "side-menu--active" : ""}`}
+              onClick={() => handleMenuItemClick("/dataList")}
+            >
               <div className="side-menu__icon"> <List /> </div>
               <div className="side-menu__title"> Data List </div>
             </NavLink>
@@ -65,7 +94,11 @@ const Sidebar = () => {
           </li>
           <li className="side-nav__devider my-6"></li>
           <li>
-            <NavLink to="/chat" activeClassName="side-menu--active" className="side-menu">
+            <NavLink
+              to="/chat"
+              className={`side-menu ${activeMenuItem === "/chat" ? "side-menu--active" : ""}`}
+              onClick={() => handleMenuItemClick("/chat")}
+            >
               <div className="side-menu__icon"> <MessageCircle /> </div>
               <div className="side-menu__title"> Chat </div>
             </NavLink>
